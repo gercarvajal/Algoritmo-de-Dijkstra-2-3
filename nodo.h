19eef9e76199a3c4d264e1981d3f91f89412bff7
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-20 00:10:26 -0300

    Create nodo.h

---
+++ nodo.h
@@ -0,0 +1,15 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
+
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdlib_h
+#endif
+
+typedef struct t_nodo{
+
+  char letra;
+  int color,peso,posicion;
+  }Nodo;
