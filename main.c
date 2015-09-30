348beefcca2868773647ad924ed632e7c6e1ed40
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-29 21:36:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define MaxNodos 8
 
+#ifndef funciones_h 
+#include <funciones.h> 
+#define funciones_h 
+#endif 
+
+#ifndef nodo_h 
+#include <nodo.h> 
+#define nodo_h 
+#endif 
 
 main(){
+  
 
   
 
