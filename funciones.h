0fac4682d9d220ad38690ee4824091934c9d672a
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-29 21:38:54 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define MaxNodos 8
 
+// Inicializa la lista de adyacencia a NULL 
+void init() {
+    int i;
+    for (i = 0; i < MaxNodos; i++)
+        Lista[i] = NULL;    
+}
+
+void mostrar_lista_adyacencia () {
+   Tnodo *q;
+   int i;
+   
+   for (i = 0; i < Num_Vertices; i++) {
+      if (Lista[i] != NULL) {
+         q = Lista[i];
+         printf ("vertice %d: ", i);
+         while (q) {
+            printf ("%d ", q->etiqueta);
+            q = q->sig;
+         }
+         printf("\n");
+      }
+   }
+}
